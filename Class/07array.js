28e355564224f25ba61917708470e2442edb81f5
Anurag Londhe <anuraglondhe9052.com>
2026-02-02 19:48:34 +0530

    Adding Array file

---
+++ Class/07array.js
@@ -0,0 +1,13 @@
+let cars = ["BMW", "HONDA", "KIA", "TATA"];
+let cars2 = ["AUDI", "MERCEDES", "PORSCHE"];
+cars.push(cars2);
+console.log(cars);
+console.log(cars[2]);
+console.log(cars[4]);
+console.log(cars[2][2]);
+
+
+console.log("-----------Concat----------");
+
+console.log(cars.concat(cars2));
+console.log(cars);
