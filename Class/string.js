cd2c4ac9da1d210124316596e5644bf969863d6b
Anurag Londhe <anuraglondhe9052.com>
2026-02-02 20:10:33 +0530

    Adding string file

---
+++ Class/string.js
@@ -0,0 +1,19 @@
+const str1 = "JS";
+console.log(str1);
+console.log("Hello "+str1);  ///old 
+
+//string interpolation
+console.log(`Hello my courseName is ${str1}`); //This is good way to use print
+
+const gameCount = new String("JSCWS");
+console.log(gameCount.length);
+console.log(gameCount[1]);//Check position
+
+console.log(gameCount.toUpperCase);
+console.log(gameCount.toLowerCase);
+console.log(gameCount.charAt[3]);//check position of index -- gives character
+console.log(gameCount.indexOf);//check index position
+
+const Fname=new String ("    Anurag    ");
+console.log(Fname);//With space
+console.log(Fname.trim);//Print Without Space  // Extra space remove
